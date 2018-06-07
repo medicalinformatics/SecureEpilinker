66880b04559df79930b9adfa2aab5563d71a3f85
Tobias Kussel <kussel@cbs.tu-darmstadt.de>
2018-06-07 17:33:45 +0200

    Added String Split functions

--- include/util.h
+++ include/util.h
@@ -29,6 +29,8 @@
 #include <stdexcept>
 #include <cctype>
 #include <locale>
+#include <sstream>
+#include <iterator>
 
 namespace sel {
 
@@ -170,6 +172,22 @@ static inline std::string trim_copy(std::string s) {
     trim(s);
     return s;
 }
+
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
+
 } // namespace sel
 
 #endif /* end of include guard: SEL_UTIL_H */
