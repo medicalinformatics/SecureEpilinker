b8fb75f415f3019b8cd9f756d8ad1ea4019c6c56
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2018-08-03 13:24:14 +0200

    Move ceil_log2{,_min1}() into math.h; add ceil_div()

--- include/math.cpp
+++ include/math.cpp
@@ -1,5 +1,5 @@
 /**
- \file    sel/math.h
+ \file    sel/math.cpp
  \author  Sebastian Stammler <sebastian.stammler@cysec.de>
  \copyright SEL - Secure EpiLinker
       Copyright (C) 2018 Computational Biology & Simulation Group TU-Darmstadt
@@ -16,33 +16,38 @@
  \brief Math tools
 */
 
-#ifndef SEL_MATH_H
-#define SEL_MATH_H
-#pragma once
-
 namespace sel {
 
-template<typename T>
-T factorial(T x) {
-  T y{1};
-  for(T i = 1; i != x; ++i)
-    y *= i;
+// https://stackoverflow.com/questions/3272424/compute-fast-log-base-2-ceiling
+int ceil_log2(unsigned long long x)
+{
+  static const unsigned long long t[6] = {
+    0xFFFFFFFF00000000ull,
+    0x00000000FFFF0000ull,
+    0x000000000000FF00ull,
+    0x00000000000000F0ull,
+    0x000000000000000Cull,
+    0x0000000000000002ull
+  };
+
+  int y = (((x & (x - 1)) == 0) ? 0 : 1);
+  int j = 32;
+  int i;
+
+  for (i = 0; i < 6; i++) {
+    int k = (((x & t[i]) == 0) ? 0 : j);
+    y += k;
+    x >>= k;
+    j >>= 1;
+  }
+
   return y;
 }
 
-/**
- * Uprounding integer division, avoiding overflow
- */
-template<typename T> constexpr
-T ceil_div(T num, T den) {
-  return 1 + ((num-1) / den);
+int ceil_log2_min1(unsigned long long x) {
+  int res = ceil_log2(x);
+  if (res < 1) res = 1;
+  return res;
 }
 
-/**
- * Ceiling of integer log2
- */
-int ceil_log2(unsigned long long x);
-int ceil_log2_min1(unsigned long long x);
-
 } // namespace sel
-#endif /* end of include guard: SEL_MATH_H */
--- include/util.cpp
+++ include/util.cpp
@@ -30,36 +30,6 @@ using namespace std;
 
 namespace sel {
 
-// https://stackoverflow.com/questions/3272424/compute-fast-log-base-2-ceiling
-int ceil_log2(unsigned long long x)
-{
-  static const unsigned long long t[6] = {
-    0xFFFFFFFF00000000ull,
-    0x00000000FFFF0000ull,
-    0x000000000000FF00ull,
-    0x00000000000000F0ull,
-    0x000000000000000Cull,
-    0x0000000000000002ull
-  };
-
-  int y = (((x & (x - 1)) == 0) ? 0 : 1);
-  int j = 32;
-  int i;
-
-  for (i = 0; i < 6; i++) {
-    int k = (((x & t[i]) == 0) ? 0 : j);
-    y += k;
-    x >>= k;
-    j >>= 1;
-  }
-
-  return y;
-}
-
-int ceil_log2_min1(unsigned long long x) {
-  return max(1, ceil_log2(x));
-}
-
 std::vector<uint8_t> vector_bool_to_bitmask(const std::vector<bool>& vb) {
   vector<uint8_t> v;
   size_t size_bytes{bitbytes(vb.size())};
