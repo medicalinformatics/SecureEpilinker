4469b1dcc63430d6a2cf1bd7bfced3cf7ce66861
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2018-08-22 11:10:35 +0200

    parse_json_field: check sizes and only return FieldEntry

--- include/jsonutils.h
+++ include/jsonutils.h
@@ -27,7 +27,7 @@
 
 namespace sel {
 
-std::pair<FieldName, FieldEntry> parse_json_field(const ML_Field&, const nlohmann::json&);
+FieldEntry parse_json_field(const ML_Field&, const nlohmann::json&);
 std::map<FieldName,FieldEntry> parse_json_fields(const std::map<FieldName, ML_Field>&, const nlohmann::json&);
 std::map<FieldName, std::vector<FieldEntry>> parse_json_fields_array(
     const std::map<FieldName, ML_Field>& fields, const nlohmann::json& json);
