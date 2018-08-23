07562c0c0a8440eb375a5bbe31e7eef77c735831
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2018-08-23 18:59:35 +0200

    :fire: gadgets: delete defunct reinterpret_share()

--- include/aby/gadgets.h
+++ include/aby/gadgets.h
@@ -55,12 +55,6 @@ void print_share(const ArithQuotient& q, const std::string& msg);
 void print_share(const BoolQuotient& q, const std::string& msg);
 
 /**
- * Creates a BoolShare with bitlen wires from an arith share with 1 wire and
- * bitlen-sized values - to be used with MUX to add flow to arithmetic shares
- */
-BoolShare reinterpret_share(const ArithShare& a, BooleanCircuit* bc);
-
-/**
  * Accumulates all values in simd_share as if it was first split and then
  * binary-accumulated using op. Returns a share of same bitlen and nvals=1.
  *
