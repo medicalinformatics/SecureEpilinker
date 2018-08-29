9576169d27ca445207e7d89882240ece3252ccf0
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2018-08-29 16:13:12 +0200

    :wrench: change default port to 8161; use ssl, gmw

--- test_scripts/r_init_tuda1.sh
+++ test_scripts/r_init_tuda1.sh
@@ -1,1 +1,1 @@
-curl -v -k -H "Expect:" -H "Content-Type: application/json" --data @configurations/remote_init_TUDA1.json -X PUT 'https://localhost:8080/initRemote/TUDA1'
+curl -v -k -H "Expect:" -H "Content-Type: application/json" --data @configurations/remote_init_TUDA1.json -X PUT 'https://localhost:8161/initRemote/TUDA1'
