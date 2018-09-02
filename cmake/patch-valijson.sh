88804691216f66bb048f7b97a9b4b7a0ba0141ac
Sebastian Stammler <stammler.s@gmail.com>
2018-09-02 23:02:55 +0200

    :ambulance: fix Release build * include OpenSSL after restbed to fix its broken build * fix valijson cmake patch and execute immediately

--- cmake/patch-valijson.sh
+++ cmake/patch-valijson.sh
@@ -10,7 +10,7 @@ find "$SOURCE_PATH" -type f \( -name "nlohmann*.hpp" \) \
     -exec sed -i -e 's@^#include <json.hpp@#include <nlohmann/json.hpp@' {} \;
 
 if ! grep -q valijson_INSTALL_HEADERS "${SOURCE_PATH}/CMakeLists.txt"; then
-  sed -i -E -e 's/(INSTALL_HEADERS|BUILD_EXAMPLES|BUILD_TESTS)/valijson_\1/p' \
+  sed -i -E -e 's/(INSTALL_HEADERS|BUILD_EXAMPLES|BUILD_TESTS)/valijson_\1/g' \
     "${SOURCE_PATH}/CMakeLists.txt"
 fi
 
