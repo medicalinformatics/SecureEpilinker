a5fc0b841b5fb60ee9ee8656ac2f7297e5246b72
Sebastian Stammler <stammler.s@gmail.com>
2018-09-02 14:51:08 +0200

    :hammer: patch valijson cmake to prefix options

--- cmake/patch-valijson.sh
+++ cmake/patch-valijson.sh
@@ -4,7 +4,14 @@ set -e
 
 SOURCE_PATH="$1"
 
-echo "Patching valijson source: $SOURCE_PATH"
+echo "Patching valijson: $SOURCE_PATH"
+
 find "$SOURCE_PATH" -type f \( -name "nlohmann*.hpp" \) \
     -exec sed -i -e 's@^#include <json.hpp@#include <nlohmann/json.hpp@' {} \;
+
+if ! grep -q valijson_INSTALL_HEADERS "${SOURCE_PATH}/CMakeLists.txt"; then
+  sed -i -E -e 's/(INSTALL_HEADERS|BUILD_EXAMPLES|BUILD_TESTS)/valijson_\1/p' \
+    "${SOURCE_PATH}/CMakeLists.txt"
+fi
+
 touch "$SOURCE_PATH/patched"
