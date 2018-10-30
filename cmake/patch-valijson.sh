9603b154e265d2f66afca2667f32e5d8f48f1d8e
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2018-10-30 11:19:00 +0100

    :arrow_up: valijson :fire: remove cmake patch

--- cmake/patch-valijson.sh
+++ cmake/patch-valijson.sh
@@ -9,9 +9,4 @@ echo "Patching valijson: $SOURCE_PATH"
 find "$SOURCE_PATH" -type f \( -name "nlohmann*.hpp" \) \
     -exec sed -i -e 's@^#include <json.hpp@#include <nlohmann/json.hpp@' {} \;
 
-if ! grep -q valijson_INSTALL_HEADERS "${SOURCE_PATH}/CMakeLists.txt"; then
-  sed -i -E -e 's/(INSTALL_HEADERS|BUILD_EXAMPLES|BUILD_TESTS)/valijson_\1/g' \
-    "${SOURCE_PATH}/CMakeLists.txt"
-fi
-
 touch "$SOURCE_PATH/patched"
