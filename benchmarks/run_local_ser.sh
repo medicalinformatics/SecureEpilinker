7e6fd00cf93a53e61b98431738702dbd7feef726
Tobias Kussel <kussel@cbs.tu-darmstadt.de>
2019-02-13 13:49:38 +0100

    :wrench: :construction: adapting benchmark scripts to SEL testing

--- benchmarks/run_local_ser.sh
+++ benchmarks/run_local_ser.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 #
 # Run me like
-# $ Q=5 N=1000 ./run_local_par.sh 3
+# $ D=5 N=1000 ./run_local_par.sh 3
 # to run three (pairs of) local instances serially with the optional parameters
-# Q, N, S prepended as environment variables
-# assumes example directory=binary name to be 'variant_query'
+# D, N, S, C prepended as environment variables
+# assumes example directory=benchmarks name to be 'test_sel'
 # Writes output to a file with all parameters included in their file names
 # Average CPU load and max. memory (RSS - resident set size) is written at the
 # end of the file
 #
 # Processes are started in local shell and can be stopped with Ctrl+C
 
-exe="../../../bin/variant_query.exe"
-Q=${Q:-1}      # Number of queries
-N=${N:-100000} # Size of a single entry
-S=${S:-5}      # Size of a query
-#P=${P:-6650}   # Port base - for parallel runs
+exe="../build/test_sel"
+D=${D:-1}      # Number of records in database
+N=${N:-1}      # Number of records to link
+C=${C:-""}     # Use Arithmetic circuit
+S=${S:-1}      # Boolean Sharing to use, 0: GMW 1: YAO
 R=${1:-1}      # Number of seriell runs
-K=${K:-32}     # dictionary key bit length
-V=${V:-16}     # dictionary value bit length
+
+if [[ ${C} ]]
+then
+  C="-c"
+fi
 
 ts=$(date +%s)
-out_base="$(hostname)_${ts}_N${N}_Q${Q}_S${S}_R${R}_ser"
+arith=${C+1}
+out_base="$(hostname)_${ts}_D${D}_N${N}_S${S}_C${arith-0}_sel"
 dir="runs/${out_base}"
-session="vq-local-${ts}"
+session="sel-local-${ts}"
 mkdir -p $dir
 
-cmd="$exe -q $Q -n $N -s $S -v $V -k $K"
-cmd_time="/usr/bin/time -f '#TimeStats\nAvgCPU\t%P\nMaxMem\t%M' -a -o"
+cmd="$exe ${C} -s ${S} -n ${D} -N ${N}"
+cmd_time="/usr/bin/time -f '[TimeStats]\nAvgCPU=\"%P\"\nMaxMem=%M' -a -o"
 echo "cmd=$cmd"
 
 # Arg 1: role (0 or 1)
@@ -37,7 +41,7 @@ run-role() {
   for r in $(seq $R); do
     echo "********** Run ${r} of ${R} **********"
     out1="${out}_r${r}"
-    eval "${cmd_time} ${out1} $cmd -r ${1} | tee ${out1}"
+    eval "${cmd_time} ${out1} $cmd -r $1 -B ${out1}| tee ${out1}"
   done
 }
 
@@ -61,3 +65,6 @@ wait $pid1
 # Weirdly, sometimes time prints the string 'Command terminated by signal 13',
 # so we have to manually delete it afterward :( E.g. with
 # $ sed -i -e '/Command terminated by signal 13/d' *
+# TK: Signal 13 means "broken pipe", i.e. something is written to a pipe 
+# where nothing is read from anymore (e.g. see 
+# http://people.cs.pitt.edu/~alanjawi/cs449/code/shell/UnixSignals.htm ).
