62fd030870ac62f85ed440ccc522f539c3ae4b9c
Sebastian Stammler <stammler.s@gmail.com>
2019-04-06 20:37:44 +0200

    :stopwatch: :sparkles: add sorting to parser

--- benchmarks/parse_for_pub.sh
+++ benchmarks/parse_for_pub.sh
@@ -4,7 +4,8 @@
 
 groupby=parameters.boolSharing,parameters.arithConversion,parameters.dbSize,parameters.numRecords
 splitby=parameters.boolSharing,parameters.arithConversion
+sortby=parameters.dbSize
 pubdir=../../2018-secure-record-linkage/data/
 pattern=sizes
 
-./parse_runs.py -C $groupby -S $splitby -o "${pubdir}${pattern}" "runs/${pattern}"*
+./parse_runs.py -C $groupby -S $splitby -s $sortby -F -o "${pubdir}${pattern}" "runs/${pattern}"*
