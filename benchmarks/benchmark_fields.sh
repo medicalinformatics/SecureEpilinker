ef9a0bfa23ab29a44640837228b25b9e36283c1c
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2019-04-24 20:37:32 +0200

    :stopwatch: :sparkles: add loop to fields benchmark

--- benchmarks/benchmark_fields.sh
+++ benchmarks/benchmark_fields.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # run benchmarks for different number of fields
-# 1) Config: 0 dkfz, 1 integer fields, 2 bitmask fields, 3 integers & bitmasks
-# 2) number of fields
+# 1) Mode M: 0 dkfz, 1 integer fields, 2 bitmask fields, 3 integers & bitmasks
+# 2) Number of fields F
 
 server=phi0
 client=phi1
-export T="combined_1" M=3 N=1 D=1000 F=1
+tag="bitmask"
+export T M=2 N=1 D=1000 F
 cmd="./run_remote_ser.sh 5 $server $client"
-C=1 S=0 $cmd
+for F in 1 2 4 8 16 32 64 128 256; do
+  echo Running benchmark for ${tag}/F=${F}
+  T="${tag}_${F}" C=1 S=0 $cmd
+done
