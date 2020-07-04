a1dbc5634cf86fec3aa96129465e23717d43c4c5
Sebastian Stammler <seb@perun.network>
2020-07-05 01:17:49 +0200

    :sparkles: Add IO structs to pysel

--- pysel/pysel.cpp
+++ pysel/pysel.cpp
@@ -17,15 +17,48 @@
 */
 
 #include "../include/clear_epilinker.h"
+#include "../include/epilink_result.hpp"
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
+using namespace sel;
 
 PYBIND11_MODULE(pysel, m) {
   m.doc() = "Clear-text EpiLinker";
 
-  m.def("calc_integer", &sel::clear_epilink::calc_integer,
+  // This creates bindings for the Input struct. Record and VRecord can directly
+  // be created from python, since they are built from stl types:
+  // Record  = map<string, optional<vector<uint8_t>>>
+  // VRecord = map<string, vector<optional<vector<uint8_t>>>>
+  py::class_<clear_epilink::Input>(m, "Input")
+    .def(py::init<const Record&, const VRecord&>());
+
+  py::class_<EpilinkConfig>(m, "EpilinkConfig")
+    .def(py::init<
+          std::map<FieldName, FieldSpec>,
+          std::vector<IndexSet>,
+          double, double
+        >());
+
+  py::class_<CircuitConfig>(m, "CircuitConfig")
+    .def(py::init<const EpilinkConfig&>());
+
+  py::class_<Result<CircUnit>>(m, "ResultInt")
+    .def_readonly("index", &Result<CircUnit>::index)
+    .def_readonly("match", &Result<CircUnit>::match)
+    .def_readonly("tmatch", &Result<CircUnit>::tmatch)
+    .def_readonly("sum_field_weights", &Result<CircUnit>::sum_field_weights)
+    .def_readonly("sum_weights", &Result<CircUnit>::sum_weights);
+  m.def("calc_integer", &clear_epilink::calc_integer,
       "Calculates the EpiLink score using the 32-bit fixed-point circuit.");
-  m.def("calc_exact", &sel::clear_epilink::calc_exact,
+
+  py::class_<Result<double>>(m, "ResultDouble")
+    .def_readonly("index", &Result<double>::index)
+    .def_readonly("match", &Result<double>::match)
+    .def_readonly("tmatch", &Result<double>::tmatch)
+    .def_readonly("sum_field_weights", &Result<double>::sum_field_weights)
+    .def_readonly("sum_weights", &Result<double>::sum_weights);
+  m.def("calc_exact", &clear_epilink::calc_exact,
       "Calculates the EpiLink score using double-precision floats.");
 }
