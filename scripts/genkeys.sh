d768c7d6d1192cfed8b8ad314bc8f1070533828c
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2023-10-16 13:30:06 +0000

    increase example certificate and dh parameter size

--- scripts/genkeys.sh
+++ scripts/genkeys.sh
@@ -2,8 +2,8 @@
 
 target_dir=${1:-../data}
 
-openssl req -new -newkey rsa:1024 -days 3650 -nodes -x509 \
+openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 \
     -subj "/C=DE/ST=Hess/L=Darmstadt/O=TUDA/CN=sel.local" \
     -keyout "${target_dir}/server.key" \
     -out "${target_dir}/server.crt" \
-&& openssl dhparam -out "${target_dir}/dh768.pem" 768
+&& openssl dhparam -out "${target_dir}/dh.pem" 2048
